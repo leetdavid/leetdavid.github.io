3da88c2b629d60a5edd64f84de2a528f52ea9807
David Lee <leetdavidu@gmail.com>
2023-12-08 11:01:03 +0800

    added PC specs type

---
+++ src/lib/types.ts
@@ -0,0 +1,12 @@
+export interface PCSpecs {
+  case: string;
+  storage: string[];
+  motherboard: string;
+  cpu: string;
+  gpu: string;
+  cooler: string;
+  ram: string[];
+  mouse: string;
+  keyboard: string;
+  monitor: string[];
+}
